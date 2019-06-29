c11259037324f3446ed19a99b0f2644fa859da90
Thai Thien <ttpro1995@yahoo.com.vn>
2019-06-29 14:14:05 +0700

    add random crop

--- data_sequence.py
+++ data_sequence.py
@@ -12,11 +12,30 @@ def load_density(file_path):
     return groundtruth
 
 
+def random_crop(img, density_map, random_crop_size):
+    """
+    adapt from https://jkjung-avt.github.io/keras-image-cropping/
+    :param img: image matrix (h, w, channel)
+    :param density_map: (h, w, channel)
+    :param random_crop_size: h_crop, w_crop
+    :return:
+    """
+    # Note: image_data_format is 'channel_last'
+    assert img.shape[2] == 3
+    height, width = img.shape[0], img.shape[1]
+    dy, dx = random_crop_size
+    x = np.random.randint(0, width - dx + 1)
+    y = np.random.randint(0, height - dy + 1)
+
+    return img[y:(y+dy), x:(x+dx), :], density_map[y:(y+dy), x:(x+dx), :]
+
+
 class DatasetSequence(Sequence):
 
-    def __init__(self, image_path_list, density_path_list):
+    def __init__(self, image_path_list, density_path_list, random_crop_size=None):
         self.image_path_list = image_path_list
         self.density_path_list = density_path_list
+        self.random_crop_size = random_crop_size
 
     def __len__(self):
         return len(self.image_path_list)
@@ -27,9 +46,12 @@ class DatasetSequence(Sequence):
 
         density = load_density(density_path)
         image = np.array(Image.open(image_path, "r").convert("RGB"))
+        density = np.expand_dims(density, axis=3)  # add channel dim
+
+        if self.random_crop_size is not None:
+            image, density = random_crop(image, density, self.random_crop_size)
 
         image = np.expand_dims(image, axis=0) # add batch dim
         density = np.expand_dims(density, axis=0) # add batch dim
-        density = np.expand_dims(density, axis=3) # add channel dim
 
         return image, density
--- test_data_sequence.py
+++ test_data_sequence.py
@@ -7,6 +7,7 @@ def test_data_sequence():
 
     :return:
     """
+    print("test_data_sequence")
     image_list = ["test_data/image/1.jpg", "test_data/image/2.jpg"]
     density_list = ["test_data/density/1.h5", "test_data/density/2.h5"]
     dataset = DatasetSequence(image_list, density_list)
@@ -14,7 +15,21 @@ def test_data_sequence():
         print(image.shape)
         print(density.shape)
         print("--------")
+    print("end test data sequence")
+
+
+def test_data_sequence_with_random_crop():
+    print("test_data_sequence_with_random_crop")
+    image_list = ["test_data/image/1.jpg", "test_data/image/2.jpg"]
+    density_list = ["test_data/density/1.h5", "test_data/density/2.h5"]
+    dataset = DatasetSequence(image_list, density_list, random_crop_size=(224, 224))
+    for image, density in dataset:
+        print(image.shape)
+        print(density.shape)
+        print("--------")
+    print("end test_data_sequence_with_random_crop")
 
 
 if __name__ == "__main__":
     test_data_sequence()
+    test_data_sequence_with_random_crop()
